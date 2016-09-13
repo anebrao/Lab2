1a08b06ef89e30d26394e731e29e902ceecb52d8
alexianebrao <alexianebrao@wifi-roaming-128-4-105-240.host.udel.edu>
2016-09-13 08:45:05 -0400

    Added card fields and constructors

--- RioBLL/src/main/java/pokerBase/Card.java
+++ RioBLL/src/main/java/pokerBase/Card.java
@@ -3,6 +3,18 @@ package pokerBase;
 import java.util.Comparator;
 
 public class Card {
+	
+	// fields 
+	private  int cardnumber;
+	private int cardsuit;
+	private int cardrank;
+	// constructors 
+	public Card(int number, int suit, int rank){
+		cardnumber = number;
+		cardsuit = suit;
+		cardrank = rank;
+	}
+
 
 	
 	
